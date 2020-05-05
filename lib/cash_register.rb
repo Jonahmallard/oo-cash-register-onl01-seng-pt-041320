8aa065508b0fdbb3ec4b374fceaeb9e436bfe088
Jonah Mallard <benjaminjmallard@gmail.com>
2020-05-05 15:24:47 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,4 +9,10 @@ class CashRegister
     self.total += price * quantity 
   end
   
+  def apply_discount 
+    if discount != 0 
+      self.total = (total * ((100.0 - discount.to_f)/100)).to_i
+    end 
+  end
+  
 end
